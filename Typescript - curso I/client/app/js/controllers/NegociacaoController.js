e2203e17cd4ab8c3c8499bc1d9d5d51fddda8035
Alexandre Ximenes <xyymenes@gmail.com>
2019-07-03 22:49:06 -0300

    tipos em typescript

--- Typescript - curso I/client/app/js/controllers/NegociacaoController.js
+++ Typescript - curso I/client/app/js/controllers/NegociacaoController.js
@@ -1,12 +1,20 @@
 class NegociacaoController {
     constructor() {
+        this._negociacoes = new Negociacoes();
         this._inputData = document.querySelector('#data');
         this._inputQuantidade = document.querySelector('#quantidade');
         this._inputValor = document.querySelector('#valor');
     }
     adiciona(event) {
         event.preventDefault();
-        let negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        let negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
+        this._negociacoes.adiciona(negociacao);
         console.log(negociacao);
+        this._negociacoes.paraArray().forEach(n => {
+            console.log(n.data);
+            console.log(n.quantidade);
+            console.log(n.valor);
+            console.log(n.volume);
+        });
     }
 }
--- Typescript - curso I/client/app/ts/controllers/NegociacaoController.ts
+++ Typescript - curso I/client/app/ts/controllers/NegociacaoController.ts
@@ -1,24 +1,32 @@
 class NegociacaoController{
 
-    private _inputData;
-    private _inputQuantidade;
-    private _inputValor
+    private _inputData: HTMLInputElement;
+    private _inputQuantidade: HTMLInputElement;
+    private _inputValor: HTMLInputElement;
+    private _negociacoes = new Negociacoes();
 
     constructor(){
-        this._inputData = document.querySelector('#data');
-        this._inputQuantidade = document.querySelector('#quantidade');
-        this._inputValor = document.querySelector('#valor');
+        this._inputData = <HTMLInputElement>document.querySelector('#data');
+        this._inputQuantidade = <HTMLInputElement>document.querySelector('#quantidade');
+        this._inputValor = <HTMLInputElement>document.querySelector('#valor');
     }
 
-    adiciona(event){
+    adiciona(event: Event){
         event.preventDefault();
         let negociacao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value,
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value),
         );
 
+        this._negociacoes.adiciona(negociacao);
         console.log(negociacao);
+        this._negociacoes.paraArray().forEach(n => {
+            console.log(n.data);
+            console.log(n.quantidade);
+            console.log(n.valor);
+            console.log(n.volume);
+        });
     }
 
 }
